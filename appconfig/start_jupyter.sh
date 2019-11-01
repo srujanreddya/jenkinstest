136537ef6fe32a91f48841a47369a6054b718844
Srujana Attunuri <srujana.attunuri@hpe.com>
2019-10-31 21:58:56 -0700

    new

--- appconfig/start_jupyter.sh
+++
@@ -1,4 +0,0 @@
-source /etc/profile.d/updatePath.sh
-mkdir -p /opt/bluedata/jupyter
-jupyter-notebook --no-browser --allow-root </dev/null & >>/var/log/jupyter-notebook.log
-echo $! > /var/run/jupyter-notebook.pid
--- appconfig/start_jupyterhub.sh
+++
@@ -1,5 +0,0 @@
-source /etc/profile.d/updatePath.sh
-source activate tensorflow3 
-setfacl -m g:Domain\ Users:rwx /home/
-jupyterhub -f /etc/jupyterhub/jupyterhub_config.py  < /dev/null & >> /var/log/jupyter-hub.log
-echo $! > /var/run/jupyter-hub.pid
